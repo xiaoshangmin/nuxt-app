bca46cd65bc177a0e520d9a64006a07466c405a5
shangminxiao <shangminxiao@gmail.com>
2024-07-23 23:56:26 +0800

    u

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
-  app: { 
+  app: {
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
@@ -54,6 +54,9 @@ export default defineNuxtConfig({
     defaultLocale: 'zh-CN',
   },
   components: true,
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  compatibilityDate: '2024-04-03', 
+  devServer: {
+    host: "0.0.0.0"
+  },
+  devtools: { enabled: false }
 })
