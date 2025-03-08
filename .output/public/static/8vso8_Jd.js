8fe3d5e7425b64486ca519376ecf3fbc3159274f
xsm <shangminxiao@gmail.com>
2025-03-08 21:35:12 +0800

    u

---
+++ .output/public/static/8vso8_Jd.js
@@ -0,0 +1,1 @@
+import{bB as t}from"./DnnbbRs_.js";import{u as e}from"./Datun4mN.js";function a(a,r){const{title:l,titleTemplate:o,...m}=a;return e({title:l,titleTemplate:o,_flatMeta:m},{...r,transform(e){const a=t({...e._flatMeta});return delete e._flatMeta,{...e,meta:a}}})}export{a as u};
